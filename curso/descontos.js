35fd28496915c9d07e4e38f020a150aa9d2479bb
Leticia Lopes Lima <leticiazlzlima@gmail.com>
2024-08-19 18:38:48 -0300

    final

---
+++ curso/descontos.js
@@ -0,0 +1,14 @@
+export const descontos = [
+    {
+        codigo: 'LAILA10',
+        desconto: 10/100
+    },
+    {
+        codigo: 'LET10',
+        desconto: 10/100
+    },
+    {
+        codigo: 'PRIMEIROBROWNIE15',
+        desconto: 15/100
+    }   
+]
---
+++ nossos_produtos/produtos.js
@@ -0,0 +1,34 @@
+export const produtos = [
+    {
+        nome: 'Caixa de Brownies',
+        valor: 30,
+        serve: '2 pessoas',
+        peso: '300g',
+        pedido_minimo: '1 caixa',
+        imagem: 'caixa_brownies.jpeg'
+    },
+    {
+        nome: 'Brownie tradicional',
+        valor: 6,
+        serve: '1 pessoa',
+        peso: '70g',
+        pedido_minimo: '7 unidades',
+        imagem: 'brownies_tradicional.jpg'
+    },
+    {
+        nome: 'Bolo brownie',
+        valor: 70,
+        serve: '10 pessoas',
+        peso: '1.2kg',
+        pedido_minimo: '1 unidade',
+        imagem: 'bolo_brownie.jpg' 
+    },
+    {
+        nome: 'Brownie com nozes',
+        valor: 8,
+        serve: '1 pessoa',
+        peso: '80g',
+        pedido_minimo: '7 unidades',
+        imagem: 'brownie_nozes.jpg'
+    }
+];
