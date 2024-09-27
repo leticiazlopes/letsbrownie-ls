5397934536a5f85f958b4e05cdf314d1e9824bd5
Leticia Lopes Lima <leticiazlzlima@gmail.com>
2024-09-27 15:26:27 -0300

    :lipstick: style: adiciona elementos estilizados ao home

--- src/app/components/Body.jsx
+++ src/app/components/Body.jsx
@@ -1,9 +1,22 @@
+import Image from "next/image"
+import Button from "./Button"
+
 export default function Body () {
     return (
         <div>
           <div class="initialrod">
-          <div class="logo"><img src="imgs/logo.png" class="border rounded-2xl mb-8"></div>
-    
+            <Image 
+                src="/imgs/logo.png"
+                width={200}
+                height={100}
+                className="border rounded-2xl mb-8"
+            />
+            <Button label="Nossos produtos" href="/index" className="botao"/>
+            <Button label="Curso de confeitaria básica" href="/index" className= "botao" />
+            </div>
+            
+
+        
         </div>
     )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
+import Body from "./components/Body";
 import Header from "./components/Header";
+import "./layout"
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <Body />
     </div>
   )
 }
