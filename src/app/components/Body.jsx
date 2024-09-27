69b60f2795d0e95d06394b2268cb868b355fd591
Leticia Lopes Lima <leticiazlzlima@gmail.com>
2024-09-27 12:51:08 -0300

    :sparkles: feat: início de implementação de componentes

---
+++ src/app/components/Body.jsx
@@ -0,0 +1,9 @@
+export default function Body () {
+    return (
+        <div>
+          <div class="initialrod">
+          <div class="logo"><img src="imgs/logo.png" class="border rounded-2xl mb-8"></div>
+    
+        </div>
+    )
+}
