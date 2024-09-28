33c5f4dad0aaf6371a2df1c65164d82db273dece
Leticia Lopes Lima <leticiazlzlima@gmail.com>
2024-09-28 09:34:12 -0300

    :sparkles: feat: insere página curso

--- src/app/curso/page.js
+++ src/app/curso/page.js
@@ -1,7 +1,7 @@
 import React from 'react'; // Importando React
-import App from '../components/App'; 
-import Card from '../components/Card';
-import Produtos from '../components/Produtos';
+import App from '../../components/App'
+import Card from '../../components/Card';
+import Produtos from '../../components/Produtos';
 
 export default function Home() {
     return (
